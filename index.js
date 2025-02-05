5d5413f0427e261975ba6937002b13406332febd
AlineCoatanoan <aline.coatanoan@gmail.com>
2025-02-05 18:51:18 +0100

    image

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 import cors from 'cors';
 
+app.use(express.static('public'));
+
 const corsOptions = {
   origin: 'https://front-books-azure.vercel.app', // L'URL de ton front
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Méthodes autorisées
@@ -17,7 +19,7 @@ const books = [
     "id": 12345,
     "title": "Le Maître et Marguerite",
     "slug": "le-maitre-et-marguerite",
-    "thumbnail": "/images/maitre.jpg",
+    "image": "/images/maitre.jpg",
     "author": "Mikhaïl Boulgakov",
     "difficulty": "Difficile",
     "description": "Un roman qui mélange fantastique, réalisme et satire. L'histoire explore la présence du Diable dans la Moscou stalinienne, où des événements surnaturels bouleversent la ville. Le Maître et Marguerite, deux personnages principaux, symbolisent l'amour, la rédemption et la lutte contre le totalitarisme.",
@@ -32,7 +34,6 @@ const books = [
     "id": 8965,
     "title": "1984",
     "slug": "1984",
-    "thumbnail": "/images/1984.jpg",
     "author": "George Orwell",
     "difficulty": "Moyen",
     "description": "Un roman dystopique où l'État totalitaire surveille et manipule ses citoyens. Winston Smith, le personnage principal, se rebelle contre un régime oppressif, mais découvre que la lutte pour la liberté est bien plus compliquée qu'il ne l'imaginait.",
@@ -47,7 +48,6 @@ const books = [
     "id": 7689,
     "title": "Le Meilleur des Mondes",
     "slug": "le-meilleur-des-mondes",
-    "thumbnail": "/images/monde.jpg",
     "author": "Aldous Huxley",
     "difficulty": "Moyen",
     "description": "Un roman dystopique visionnaire qui décrit un futur où la société est parfaitement régulée, mais au prix de la liberté individuelle et de l'émotion. La pensée critique est éliminée au profit de la consommation de masse et du confort matériel.",
@@ -62,7 +62,6 @@ const books = [
     "id": 8956,
     "title": "Hamlet",
     "slug": "hamlet",
-    "thumbnail": "/images/hamlet.jpg",
     "author": "William Shakespeare",
     "difficulty": "Difficile",
     "description": "La célèbre tragédie de Shakespeare où le prince Hamlet cherche à venger la mort de son père, tué par son oncle. Un chef-d'œuvre sur la vengeance, la folie et les dilemmes moraux.",
@@ -77,7 +76,6 @@ const books = [
     "id": 7123,
     "title": "Silo",
     "slug": "silo",
-    "thumbnail": "/images/silo.jpg",
     "author": "Hugh Howey",
     "difficulty": "Moyen",
     "description": "Dans un futur post-apocalyptique, les derniers survivants de l'humanité vivent dans un silo souterrain gigantesque. Le roman suit l’histoire de personnages qui remettent en question la vérité sur leur monde enfermé.",
@@ -92,7 +90,6 @@ const books = [
     "id": 3412,
     "title": "Les Robots",
     "slug": "les-robots",
-    "thumbnail": "/images/robots.jpg",
     "author": "Isaac Asimov",
     "difficulty": "Facile",
     "description": "Une série de nouvelles qui explore les lois de la robotique et les relations entre les humains et les intelligences artificielles. Asimov pose des questions éthiques sur la technologie et la place des machines dans la société.",
